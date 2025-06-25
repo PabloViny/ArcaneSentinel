d38c0ade97c994c6a7b6f724a9d075398dd2a14a
Pablo Vinícius <pablovinissantos@hotmail.com>
2025-06-24 22:16:06 -0300

    Inserting menu navigation

--- code/Const.py
+++ code/Const.py
@@ -3,11 +3,11 @@ C_GREEN = (80, 250, 123)
 C_WHITE = (240, 240, 240)
 
 # M
-MENU_OPTION = ['NEW GAME 1P',
+MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
-               'EXIT']
+               'EXIT')
 
 # W
 WIN_WIDTH = 576
