39413c66df3b5853283bb2a37f342d09e2e1d960
Pablo Vinícius <pablovinissantos@hotmail.com>
2025-06-25 18:54:06 -0300

    Fixing imports in Level.py file

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -8,5 +8,7 @@ class EntityFactory:
     def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
             case 'Level1Bg':
-                bg = Background('Level1Bg', (0, 0))
-                return bg
+                list_bg = []
+                for i in range(1):
+                    list_bg.append(Background(f'Level1Bg', (0, 0)))
+                return list_bg
